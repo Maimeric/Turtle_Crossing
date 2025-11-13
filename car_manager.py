270a7de43733fec9b3b46e4bbb760f8683af5b5a
Artsem <artiom.levitskiy@gmail.com>
2025-11-12 20:08:44 -0500

    Creating car class and movement for the cars

--- car_manager.py
+++ car_manager.py
@@ -2,6 +2,19 @@ COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+from turtle import Turtle
+import random
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color(random.choice(COLORS))
+        self.shape("square")
+        self.shapesize(stretch_wid= 1, stretch_len= 2)
+        self.penup()
+        self.goto(300, random.randint(-250,250))
+        self.left(180)
+
+
+    def move(self):
+        self.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -13,8 +13,20 @@ player = Player()
 screen.listen()
 screen.onkey(player.move, "Up")
 
+cars = []
+iteration_count = 0
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if iteration_count == 6:
+        cars.append(CarManager())
+        iteration_count = 0
+
+    for i in cars:
+        i.move()
+
+    iteration_count += 1
